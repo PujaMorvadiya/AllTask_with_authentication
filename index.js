4fa00c8f6f0134bea89a006ae2a815e776a8e4c2
Puja Moravadiya <pujamorvadiya1242@gmail.com>
2024-03-31 18:32:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+const all = require("./src/routes/router");
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/img_videos"));
+
+let port = process.env.PORT || 5065;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(
+      `Error: other server is running in  ${port} ,change the port number`
+    );
+  } else {
+    console.log(`Server is running in port: ${port} `);
+    app.use(all);
+  }
+});
---
+++ src/controller/checkdate.js
@@ -0,0 +1,16 @@
+var con = require("../models/database");
+
+function checkmail(req, res) {
+  console.log(req.query.email);
+  con.query(
+    `select * from login where email='${req.query.email}'`,
+    function (err, result) {
+      if (err) throw err;
+      data = result;
+      console.log("result", result);
+      res.json(data);
+    }
+  );
+}
+
+module.exports = checkmail;
